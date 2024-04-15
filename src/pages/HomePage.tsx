c9a3a6f6bb5a2d153720ebb0a48ac35e98769f59
marcothelost <marcothelost@proton.me>
2024-04-15 15:39:17 +0200

    Creating the Page Sidebar block

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 
+import { PageSidebar } from '../components/blocks/PageSidebar';
+
 export const HomePage: React.FC = () => (
   <React.Fragment>
+    <PageSidebar />
     <h1>Home</h1>
   </React.Fragment>
 );
